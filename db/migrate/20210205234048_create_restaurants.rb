1a3ac1e8f4285908242d2a384a372f9afe40dbc3
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-05 23:29:24 -0500

    Resolve built-in attribute name conflict 'type'

--- db/migrate/20210205234048_create_restaurants.rb
+++ db/migrate/20210205234048_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :type
+      t.string :cuisine
       t.integer :user_id
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210205212006) do
+ActiveRecord::Schema.define(version: 20210206035500) do
+
+  create_table "dishes", force: :cascade do |t|
+    t.string  "name"
+    t.date    "date"
+    t.float   "rating"
+    t.integer "restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string  "name"
+    t.string  "cuisine"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
