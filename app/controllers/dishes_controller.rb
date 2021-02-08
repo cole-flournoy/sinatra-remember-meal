937b7a8ae689e15dd39acab88f9ce272a229fbd9
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-08 11:17:15 -0500

    Stub dish controller CRUD actions

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,30 @@
+class DishesController < ApplicationController
+    get '/dishes' do
+    # erb :'/dishes/index'
+    end
+
+    post '/dishes' do 
+    
+    end
+
+    get '/dishes/new' do
+    # erb :'/dishes/new'
+    end
+
+    get '/dishes/:id' do 
+    # erb :'/dishes/show'
+    end
+
+    patch '/dishes/:id' do 
+    
+    end
+
+    delete '/dishes/:id' do
+        
+    end
+
+    get '/dishes/:id/edit' do 
+    # erb :'/dishes/edit'
+    end
+
+end
