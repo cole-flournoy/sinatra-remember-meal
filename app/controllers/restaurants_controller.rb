b304868620b232a09dd3aa9b7355cb5bb03a9d08
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-08 11:14:44 -0500

    Stub restaurant controllerCRUD actions

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+    get '/restaurants' do
+    # erb :'/restaurants/index'
+    end
+
+    post '/restaurants' do 
+    
+    end
+
+    get '/restaurants/new' do
+    # erb :'/restaurants/new'
+    end
+
+    get '/restaurants/:id' do 
+    # erb :'/restaurants/show'
+    end
+
+    patch '/restaurants/:id' do 
+    
+    end
+
+    delete '/restaurants/:id' do
+        
+    end
+
+    get '/restaurants/:id/edit' do 
+    # erb :'/restaurants/edit'
+    end
+
+end
