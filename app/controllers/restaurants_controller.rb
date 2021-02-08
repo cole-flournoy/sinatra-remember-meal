81837866e39f1c5e2398a69c2918e3fa36757092
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-08 16:17:34 -0500

    Create and connect restaurant CRUD views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,32 +5,41 @@ class RestaurantsController < ApplicationController
     end
     
     get '/restaurants' do
+        # make this only the user's restaurants
+        # main index at '/' should be all users' restaurants
         @restaurants = Restaurant.all
         erb :index
     end
 
     post '/restaurants' do 
-    
+        Restaurant.create(params)
+        redirect '/restaurants'
     end
 
     get '/restaurants/new' do
-    # erb :'/restaurants/new'
+        erb :new
     end
 
     get '/restaurants/:id' do 
-    # erb :'/restaurants/show'
-    end
-
-    patch '/restaurants/:id' do 
-    
+        # include more detail here: rating, review, visits, dishes, etc.
+        @restaurant = Restaurant.find(params[:id])
+        erb :show
     end
 
     delete '/restaurants/:id' do
-        
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        redirect '/restaurants'
     end
 
     get '/restaurants/:id/edit' do 
-    # erb :'/restaurants/edit'
+        @restaurant = Restaurant.find(params[:id])
+        erb :edit
     end
-
+    
+    post '/restaurants/:id' do 
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.update(params)
+        redirect "/restaurants/#{@restaurant.id}"
+    end
 end
