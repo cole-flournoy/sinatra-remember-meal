0079001bba302e3c5d8ec5dac0bc75131bd6e895
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-09 00:09:09 -0500

    Add CRUD views for dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,30 +1,53 @@
 class DishesController < ApplicationController
-    get '/dishes' do
-    # erb :'/dishes/index'
+       
+    configure do
+        set :views, 'app/views/dishes'
     end
 
-    post '/dishes' do 
-    
+    get '/dishes' do
+        # only the user's dishes
+        @dishes = Dish.all
+        erb :index
     end
 
     get '/dishes/new' do
-    # erb :'/dishes/new'
+        @restaurants = Restaurant.all
+        erb :new
+    end
+
+    post '/dishes' do 
+        @dish = Dish.create(params[:dish])
+        if !params["restaurant"]["name"].empty?
+            @dish.restaurant = Restaurant.create(params[:restaurant])
+            @dish.save
+        end
+        redirect '/dishes'
     end
 
     get '/dishes/:id' do 
-    # erb :'/dishes/show'
-    end
-
-    patch '/dishes/:id' do 
-    
+        @dish = Dish.find(params[:id])
+        erb :show
     end
 
     delete '/dishes/:id' do
-        
+        dish = Dish.find(params[:id])
+        dish.destroy
+        redirect '/dishes'
     end
 
     get '/dishes/:id/edit' do 
-    # erb :'/dishes/edit'
+        @dish = Dish.find(params[:id])
+        @restaurants = Restaurant.all
+        erb :edit
     end
 
+    patch '/dishes/:id' do 
+        dish = Dish.find(params[:id])
+        dish.update(params["dish"])
+        if !params["restaurant"]["name"].empty?
+            dish.restaurant = Restaurant.create(params[:restaurant])
+            dish.save
+        end
+        redirect "/dishes/#{dish.id}"
+    end
 end
