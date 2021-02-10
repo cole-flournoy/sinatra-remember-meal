ede46080b73d07b8bc9d97d7a72d4cc625289b44
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-09 23:32:43 -0500

    Create custom error for duplicate signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   get '/signup' do
+    @error_message = params[:error]
     if !logged_in?
       erb :'users/new'
     else
@@ -9,16 +10,20 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    user = User.new(username: params[:username], password: params[:password])
-
-    if user.save
-      user.save
-      session[:user_id] = user.id
-      redirect '/restaurants'
+    existing_username = User.find_by(username: params[:username])
+    if existing_username
+      redirect '/signup?error=That username already exists'
     else
-      redirect '/signup'
-    end
+      new_user = User.new(username: params[:username], password: params[:password])
 
+      if new_user.save
+        new_user.save
+        session[:user_id] = new_user.id
+        redirect '/restaurants'
+      else
+        redirect '/signup'
+      end
+    end
   end
 
   get '/login' do
